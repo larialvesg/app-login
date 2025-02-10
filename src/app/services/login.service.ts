65bb48ba8b5182d676dcaa68532a745aa260ef79
larialvesg <larissaalvesgeraldoo@gmail.com>
2025-02-10 16:52:50 -0300

    alterações - 10/02/2025

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -7,11 +7,12 @@ import { tap } from 'rxjs';
   providedIn: 'root'
 })
 export class LoginService {
+  apiUrl: string = "http://localhost:8080/auth/login"
 
   constructor(private httpClient: HttpClient) { }
 
-  login(name: string, password:string){
-    return this.httpClient.post<LoginResponse>("/login", {name, password}).pipe(
+  login(email: string, password:string){
+    return this.httpClient.post<LoginResponse>(this.apiUrl, {email, password}).pipe(
       tap((value) => {
         sessionStorage.setItem("auth-token", value.token)
         sessionStorage.setItem("username", value.name)
